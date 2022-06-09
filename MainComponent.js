bacef2f5f18157a047be8c19855448558c0822ec
Ivan Carreño <52723118+carreivan@users.noreply.github.com>
2022-06-09 00:53:07 -0300

    Initial commit

---
+++ MainComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainComponent() {
+  return (
+    <main>
+      <h3>Este e o conteudo principal do site</h3>
+    </main>
+  );
+}
+
+export default MainComponent;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+    <ul>
+    <li>Link1</li>  
+    <li>Link2</li>  
+    <li>Link3</li>  
+    </ul>  
+    </nav>
+  );
+}
+
+export defaul Header;
